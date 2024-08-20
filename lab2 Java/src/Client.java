fcfdf187ddd7842d2e7009e8fe49996e7ad84a17
Andriilupoi <lupoi_2005@icloud.com>
2024-08-20 19:19:33 +0300

    Lab2 Java

---
+++ lab2 Java/src/Client.java
@@ -0,0 +1,19 @@
+public class Client implements TickedBookingSystemPrototype {
+    private String name;
+    private String number;
+
+    public Client(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    @Override
+    public Client clone() {
+        return new Client(this.name, this.number);
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" + "name='" + name + '\'' + ", number='" + number + '\'' + '}';
+    }
+}
---
+++ lab2 Java/src/Main.java
@@ -0,0 +1,17 @@
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+        Client client1 = new Client("Volodya", "228");
+        Movie movie1 = new Movie("Titanic", "Drama", LocalDateTime.now().plusHours(4));
+        Seat seat1 = new Seat(10, false);
+
+        Order originalOrder = new Order(client1, movie1, seat1);
+
+        Order clonedOrder = originalOrder.clone();
+
+        System.out.println("Original Order: " + originalOrder);
+        System.out.println("\n");
+        System.out.println("Cloned Order: " + clonedOrder);
+    }
+}
