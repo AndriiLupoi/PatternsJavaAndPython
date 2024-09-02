8beb4fc0ca33aa573edb85fa1da589bc7694c991
Andriilupoi <lupoi_2005@icloud.com>
2024-09-02 22:26:55 +0300

    lab3 java

--- lab2 Java/src/Main.java
+++ lab2 Java/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Client client1 = new Client("Volodya", "228");
         Movie movie1 = new Movie("Titanic", "Drama", LocalDateTime.now().plusHours(4));
-        Seat seat1 = new Seat(10, false);
+        Seat seat1 = new Seat(10, true);
 
         Order originalOrder = new Order(client1, movie1, seat1);
 
